cfe79716be248d738591a1c0ce9cd432fddc29b7
kardasch404 <zz2406143@gmail.com>
2026-01-20 20:59:16 +0100

    feat(auth): implement register use case

---
+++ src/application/use-cases/register.use-case.ts
@@ -0,0 +1,34 @@
+import { Injectable, ConflictException } from '@nestjs/common';
+import { IUserRepository } from '../../domain/interfaces/user-repository.interface';
+import { RegisterUserDto } from '../dto/register-user.dto';
+import { UserResponseDto } from '../dto/user-response.dto';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class RegisterUseCase {
+  constructor(private readonly userRepository: IUserRepository) {}
+
+  async execute(dto: RegisterUserDto): Promise<UserResponseDto> {
+    const existingUser = await this.userRepository.findByEmail(dto.email);
+    if (existingUser) {
+      throw new ConflictException('Email already exists');
+    }
+
+    const hashedPassword = await bcrypt.hash(dto.password, 10);
+
+    const user = await this.userRepository.create({
+      email: dto.email,
+      password: hashedPassword,
+      firstname: dto.firstname,
+      lastname: dto.lastname,
+    });
+
+    return {
+      id: user.id,
+      email: user.email,
+      firstname: user.firstname,
+      lastname: user.lastname,
+      createdAt: user.createdAt,
+    };
+  }
+}
