4e61c87b4114f64db822e9f2c6c52625df6c04f9
kardasch404 <zz2406143@gmail.com>
2026-01-20 20:58:43 +0100

    feat(auth): add registration DTOs

---
+++ src/application/dto/register-user.dto.ts
@@ -0,0 +1,20 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class RegisterUserDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @MinLength(8)
+  @IsNotEmpty()
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  firstname: string;
+
+  @IsString()
+  @IsNotEmpty()
+  lastname: string;
+}
---
+++ src/application/dto/user-response.dto.ts
@@ -0,0 +1,7 @@
+export class UserResponseDto {
+  id: string;
+  email: string;
+  firstname: string;
+  lastname: string;
+  createdAt: Date;
+}
