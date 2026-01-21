054dc21f28c1cf3fdab5e222daa43e79554e1fad
kardasch404 <zz2406143@gmail.com>
2026-01-21 16:16:47 +0100

    feat(auth): create permission decorator

--- src/presentation/decorators/check-permission.decorator.ts
+++ src/presentation/decorators/check-permission.decorator.ts
@@ -4,3 +4,5 @@ export const PERMISSION_KEY = 'permission';
 
 export const CheckPermission = (resource: string, action: string) =>
   SetMetadata(PERMISSION_KEY, { resource, action });
+
+export const RequirePermission = CheckPermission;
