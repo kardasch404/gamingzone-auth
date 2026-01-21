0a88f4f275116f8623f8e72098a65bf29a923906
kardasch404 <zz2406143@gmail.com>
2026-01-21 16:08:57 +0100

    feat(auth): configure passport jwt strategy

---
+++ src/infrastructure/auth/jwt.strategy.ts
@@ -0,0 +1,23 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { JwtPayload } from '../../../domain/interfaces/token-service.interface';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get('jwt.accessSecret'),
+    });
+  }
+
+  async validate(payload: JwtPayload): Promise<JwtPayload> {
+    if (!payload.sub) {
+      throw new UnauthorizedException();
+    }
+    return payload;
+  }
+}
